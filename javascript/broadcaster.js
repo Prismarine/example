7601d18709a3e2db7e1e991f6bc359c056776d2e
Nick Vernij <nickvernij@outlook.com>
2015-05-28 16:24:01 +0200

    Created first plugin example

---
+++ javascript/broadcaster.js
@@ -0,0 +1,22 @@
+//required for the server, to recognize this as a plugin.
+var plugin = {
+  'name'        : 'test',
+  'description' : 'broadcasts test',
+  'api_version' : '1',
+  'authors'     : ['Nick', 'Steve', 'Notch']
+};
+
+//json message, as being used in vanilla minecraft.
+var message = {
+  "text"  :"[Broadcast] Welcome to my awesome server, running prismarine",
+  "color" :"gold",
+  "bold"  :"true"
+};
+
+//this function is called when te plugin is being initialzied
+function onEnable() {
+  setInterval(function(){
+    //this is an example of an API function.
+    Server.broadcastJsonMessage(message);
+  }, 60000);
+}
